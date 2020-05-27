384e2d7b861e675648d03ef33c90a221f7836645
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-05-27 22:04:37 +0100

    adjusted the width of leaderboard

--- src/scenes/SceneGameOver.js
+++ src/scenes/SceneGameOver.js
@@ -69,7 +69,7 @@ class SceneGameOver extends Phaser.Scene {
         .filter((game, i) => i < 5)
         .map((game, i) => {
           const text = `Player: ${game.user.toUpperCase()} | Score: ${game.score}`;
-          this.add.text(window.global.width * 0.5, (93 * (i + 1)), text).setOrigin(0.5);
+          this.add.text(window.global.width * 0.5, (93 * (i + 1.1)), text).setOrigin(0.5);
           return text;
         });
 
